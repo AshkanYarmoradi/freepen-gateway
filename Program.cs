20d62276114e9a2408fc2d2f8e291a75acdfaa86
Ashkan Yarmoradi <ayarmoradi@outlook.com>
2025-06-23 00:41:24 +0200

    Add initial project files for FreePenGateway including configuration for YARP reverse proxy

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System.Net.Security;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add YARP services
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Enable the YARP reverse proxy
+app.MapReverseProxy();
+
+app.Run();
